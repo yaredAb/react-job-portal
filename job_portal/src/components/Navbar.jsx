f92263345aa602f804c3a076b8968c390cc2e2e6
yaredyh11 <yaredyh11@gmail.com>
2024-07-10 13:39:30 +0300

    Project ended -1

--- job_portal/src/components/Navbar.jsx
+++ job_portal/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
           <div
             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
           >
-            <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
               <img
                 className="h-10 w-auto"
                 src={logo}
@@ -21,7 +21,7 @@ const Navbar = () => {
               <span className="hidden md:block text-white text-2xl font-bold ml-2"
                 >React Jobs</span
               >
-            </a>
+            </NavLink>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
                 <NavLink
